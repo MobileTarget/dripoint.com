97408c801bfabd23e24cae80a108ac8ab8b9e950
Ubuntu <ubuntu@ip-10-170-82-115.ec2.internal>
2014-10-27 20:29:21 +0000

    Fixed onpublish.js to copy correctly.

--- build_settings/onpublish.js
+++ build_settings/onpublish.js
@@ -5,7 +5,7 @@ from_path = '/dri/driapp/build_settings/machine_name.txt';
 to_path = '/dri/machine_name.txt';
 
 function copyFromTo(fromPath, toPath) {
-        var cmd = 'cp ' + fromPath + '/ ' + toPath;
+        var cmd = 'cp ' + fromPath + ' ' + toPath;
         console.log("copyFromTo running command: " + cmd);
         exec(cmd, function(err, stdout, stderr) {
                 console.log('STDOUT = ' + stdout);
