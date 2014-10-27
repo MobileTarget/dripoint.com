988862305d6b6fc8527ef174abddb31a3e93f48e
DRI-Dev <info@dri.com>
2014-10-27 16:24:37 -0400

    Changed onpublish to copy machine_name.txt to /dri

--- build_settings/onpublish.js
+++ build_settings/onpublish.js
@@ -1,1 +1,16 @@
-// This code does nothing...
+require('fs');
+var exec = require('child_process').exec;
+
+from_path = '/dri/driapp/build_settings/machine_name.txt';
+to_path = '/dri/machine_name.txt';
+
+function copyFromTo(fromPath, toPath) {
+        var cmd = 'cp ' + fromPath + '/ ' + toPath;
+        console.log("copyFromTo running command: " + cmd);
+        exec(cmd, function(err, stdout, stderr) {
+                console.log('STDOUT = ' + stdout);
+                console.log('STERR = ' + stderr);
+        });
+}
+
+copyFromTo(from_path, to_path);
